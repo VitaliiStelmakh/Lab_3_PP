45fb20fa5a65825da6d5712a8cf155f02c0ce460
VitaliiStelmakh <94753413+VitaliiStelmakh@users.noreply.github.com>
2021-11-22 22:11:43 +0200

    Add new class DroidDefender

---
+++ src/Model/DroidDefender.java
@@ -0,0 +1,34 @@
+package Model;
+
+import java.util.Random;
+
+public class DroidDefender extends Droid{
+
+
+    public static final Random random = new Random();
+
+
+    public DroidDefender() {
+        this.name = "DroidDefender";
+        this.health = 400;
+        this.energy = 30;
+    }
+    @Override
+    public void Attack() {
+        this.setDamage((random.nextInt(30) + 20));
+    }
+
+    @Override
+    public void Defense(int damage) {
+        if(energy<10)
+            this.setHelth(this.getHealth() - damage);
+        else {
+            if (random.nextInt(3) == 0){
+                System.out.println("Block damage");
+                this.setHelth(this.getHealth());
+            }
+            else
+            this.setHelth(this.getHealth() - damage);
+        }
+    }
+}
